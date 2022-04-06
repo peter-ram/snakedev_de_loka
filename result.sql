a47647169f251ccc4bdcb4ae7f1ab4c1b43b512d
peter-ram <pedro.carneiro@loka.com>
2022-04-06 19:04:09 +0100

    creating view for BI logic to be business agnostic

--- result.sql
+++ result.sql
@@ -6,6 +6,12 @@ SELECT ride_id, started_at, ended_at,
         strftime('%w', started_at) as weekeday
 FROM bikes;
 
+DROP VIEW IF EXISTS BI_view;
+
+CREATE VIEW BI_view AS  
+SELECT weekeday, ROUND(AVG(duration_minutes),1) as "Average_Duration" FROM bikes_view
+
+GROUP BY weekeday;
 
 /* Query to perform groupby and obtain final result */
 SELECT weekeday, ROUND(AVG(duration_minutes),1) as "Average_Duration" FROM bikes_view
